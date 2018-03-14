f9cff6034c966314ad427ad07c142b4ef53defd2
Harvey <harvey@github.com>
2018-03-14 22:51:09 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+
+var one200 = $("#one200");
+one200[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+   $("#one200").text("");
+})
+
+var one400 = $("#one400");
+one400[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+  $("#one400").text("");
+})
+
+var one800 = $("#one800");
+one800[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+  $("#one800").text("");
+})
+
+var one1000 = $("#one1000");
+one1000[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#one1000").text("");
+})
+
+var two200 = $("#two200");
+two200[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#two200").text("");
+})
+
+var two400 = $("#two400");
+two400[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#two400").text("");
+})
+
+var two800 = $("#two800");
+two800[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#two800").text("");
+})
+
+var two1000 = $("#two1000");
+two1000[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#two1000").text("");
+})
+
+var three200 = $("#three200");
+three200[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#three200").text("");
+})
+
+var three400 = $("#three400");
+three400[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#three400").text("");
+})
+
+var three800 = $("#three800");
+three800[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#three800").text("");
+})
+
+var three1000 = $("#three1000");
+three1000[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#three1000").text("");
+})
+
+var four200 = $("#four200");
+four200[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+  $("#four200").text("");
+})
+
+var four400 = $("#four400");
+four400[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#four400").text("");
+})
+
+var four800 = $("#four800");
+four800[0].addEventListener("click", function(){
+  var answer = prompt("Sample");
+    $("#four800").text("");
+})
+
+var four1000 = $("#four1000");
+four1000[0].addEventListener("click", function(){
+  var answer = prompt("Sample"); 
+    $("#four1000").text("");
+})
+})
